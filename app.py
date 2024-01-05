392a683ccffc9b67ae6888e449b54d6cc16f1970
Satrio Arda <75375473+satrioaa@users.noreply.github.com>
2024-01-05 16:06:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import itertools
 import pickle
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
