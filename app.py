f7bab6b39a6a274421ae7590e5e57aac45da7cf0
Satrio Arda <75375473+satrioaa@users.noreply.github.com>
2024-01-05 15:38:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Load the dataset
 @st.cache
